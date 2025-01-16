06c5fbeb4dc14714ae2ffb1cf806a56520206e56
stormymayday <stormymayday7@gmail.com>
2025-01-15 21:56:58 -0800

    BST: insert

--- implementations/trees/bst/scott/index.ts
+++ implementations/trees/bst/scott/index.ts
@@ -14,4 +14,36 @@ export default class BST<T> {
     constructor() {
         this.root = null;
     }
+
+    insert(value: T): BST<T> | undefined {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        let temp = this.root;
+        while (true) {
+            if (newNode.value === temp.value) {
+                // Duplicate value, do not insert
+                return undefined;
+            }
+            if (newNode.value < temp.value) {
+                if (temp.left === null) {
+                    // Insert as left child
+                    temp.left = newNode;
+                    return this;
+                }
+                // Traverse left
+                temp = temp.left;
+            } else {
+                if (temp.right === null) {
+                    // Insert as right child
+                    temp.right = newNode;
+                    return this;
+                }
+                // Traverse right
+                temp = temp.right;
+            }
+        }
+    }
 }
