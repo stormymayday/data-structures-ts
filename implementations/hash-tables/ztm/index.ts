05ebfba3af372f7414e6cbaa432988e5c8ce7fad
stormymayday <stormymayday7@gmail.com>
2025-01-17 14:40:15 -0800

    ztm HT: get

--- implementations/hash-tables/ztm/index.ts
+++ implementations/hash-tables/ztm/index.ts
@@ -21,4 +21,17 @@ export default class HashTable<T> {
         this.data[address].push([key, value]);
         return this.data;
     }
+
+    get(key: string) {
+        const address = this._hash(key);
+        const currentBucket = this.data[address];
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
 }
