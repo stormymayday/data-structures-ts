a6b786f081a20892cecfbfdb5acd0f06813826ca
stormymayday <stormymayday7@gmail.com>
2025-01-17 12:37:02 -0800

    HT: hash

--- implementations/hash-tables/barrett/index.ts
+++ implementations/hash-tables/barrett/index.ts
@@ -4,4 +4,12 @@ export default class HashTable<T> {
     constructor(size: number = 7) {
         this.dataMap = new Array(size);
     }
+
+    private _hash(key: string): number {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+        }
+        return hash;
+    }
 }
