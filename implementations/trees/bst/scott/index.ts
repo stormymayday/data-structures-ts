5a19ca5cc8d7125b98aa21c806217609f9adfc62
stormymayday <stormymayday7@gmail.com>
2025-01-16 22:38:24 -0800

    BST: contains

--- implementations/trees/bst/scott/index.ts
+++ implementations/trees/bst/scott/index.ts
@@ -46,4 +46,21 @@ export default class BST<T> {
             }
         }
     }
+
+    contains(value: T): boolean {
+        if (this.root === null) {
+            return false;
+        }
+        let temp: Node<T> | null = this.root;
+        while (temp) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
