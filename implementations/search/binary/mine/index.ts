11bce869695c8d6244758de1823228ea2909d66b
stormymayday <stormymayday7@gmail.com>
2025-02-13 13:57:48 -0800

    edge case - 1 element array

--- implementations/search/binary/mine/index.ts
+++ implementations/search/binary/mine/index.ts
@@ -3,6 +3,13 @@ export default function binarySearch(arr: number[], target: number): number {
         return -1;
     }
 
+    if (arr.length === 1) {
+        // If the array has only one element, check if it's equal to the target.
+        // If it matches the target, return its index (0).
+        // Otherwise, return -1, indicating the target is not in the array.
+        return arr[0] === target ? 0 : -1;
+    }
+
     let left = 0;
     let right = arr.length - 1;
 
