eafb1f17f7d5013d7afef7b38e6e9e01926e061c
stormymayday <stormymayday7@gmail.com>
2025-02-23 19:42:17 -0800

    steele: binary search

---
+++ implementations/search/binary/steele/index.ts
@@ -0,0 +1,18 @@
+// Potential Flaw: It doesn't properly check boundary conditions when start > end, potentially causing arr[middle] to be accessed out of bounds.
+export default function binarySearch(arr: number[], elem: number): number {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+    while (arr[middle] !== elem && start <= end) {
+        if (elem < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if (arr[middle] === elem) {
+        return middle;
+    }
+    return -1;
+}
