9c29150df36627da6312085feb80e324c9722de4
stormymayday <stormymayday7@gmail.com>
2025-02-23 19:38:05 -0800

    simplified 'mine' binary search

--- implementations/search/binary/mine/index.ts
+++ implementations/search/binary/mine/index.ts
@@ -1,15 +1,4 @@
 export default function binarySearch(arr: number[], target: number): number {
-    if (!arr.length || target < arr[0] || target > arr[arr.length - 1]) {
-        return -1;
-    }
-
-    if (arr.length === 1) {
-        // If the array has only one element, check if it's equal to the target.
-        // If it matches the target, return its index (0).
-        // Otherwise, return -1, indicating the target is not in the array.
-        return arr[0] === target ? 0 : -1;
-    }
-
     let left = 0;
     let right = arr.length - 1;
 
