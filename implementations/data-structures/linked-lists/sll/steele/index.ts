69d7f0c4f49fa2dbd59953603d23f1a3c3667348
stormymayday <stormymayday7@gmail.com>
2025-02-24 16:03:52 -0800

    steele sll class constructor

--- implementations/data-structures/linked-lists/sll/steele/index.ts
+++ implementations/data-structures/linked-lists/sll/steele/index.ts
@@ -1,8 +1,19 @@
-export default class Node {
+class Node {
     val: number;
     next: Node | null;
     constructor(val: number) {
         this.val = val;
         this.next = null;
+    }
+}
+
+export default class SinglyLinkedList {
+    head: Node | null;
+    tail: Node | null;
+    length: number;
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
 }
