366df9b0515788b4a962da3438683fec9542079c
stormymayday <stormymayday7@gmail.com>
2025-02-25 17:37:04 -0800

    custom array

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
 "use client";
 
-import SinglyLinkedList from "@/implementations/data-structures/linked-lists/sll/steele";
-
-const mySLL = new SinglyLinkedList();
-console.log(mySLL);
-console.log(mySLL.push(1));
-console.log(mySLL.push(2));
-console.log(mySLL.push(3));
-
-console.log(mySLL.reverse());
-
 export default function Home() {
     return (
         <div className="h-[100vh] flex items-center justify-center u">
---
+++ implementations/data-structures/array/index.ts
@@ -0,0 +1,41 @@
+export default class CustomArray<T> {
+    data: Record<string, T>;
+    length: number;
+
+    constructor() {
+        this.data = {};
+        this.length = 0;
+    }
+
+    push() {}
+
+    pop() {}
+
+    shift() {}
+
+    unshift() {}
+
+    forEach() {}
+
+    map() {}
+
+    filter() {}
+
+    reduce() {}
+
+    slice() {}
+
+    splice() {}
+
+    concat() {}
+
+    square() {}
+
+    at() {}
+
+    includes() {}
+
+    indexOf() {}
+
+    join() {}
+}
