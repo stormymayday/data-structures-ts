c641b6ff11933894a758ffadfbe6eaf00ddfd18a
stormymayday <stormymayday7@gmail.com>
2025-02-25 19:54:35 -0800

    barrett sll push

--- implementations/data-structures/linked-lists/sll/barrett/index.ts
+++ implementations/data-structures/linked-lists/sll/barrett/index.ts
@@ -17,4 +17,24 @@ export default class LinkedList<T> {
         this.tail = this.head;
         this.length = 1;
     }
+
+    push(value: T): LinkedList<T> {
+        // Creating a new node with a given value
+        const newNode = new Node(value);
+        // (Edge Case) Checking if the list is empty
+        if (!this.head) {
+            // If empty, setting head and tail to the new node
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // Connecting current tail's next pointer to the new node
+            this.tail!.next = newNode;
+            // Moving the tail pointer to the new node
+            this.tail = newNode;
+        }
+        // Incrementing the length
+        this.length++;
+        // Returning the entire list
+        return this;
+    }
 }
