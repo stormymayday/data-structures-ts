627cd77444c2ff147f9c2dc525174250998dd458
stormymayday <stormymayday7@gmail.com>
2025-04-12 14:08:26 -0700

    barrett dll push

--- implementations/data-structures/linked-lists/dll/barrett/index.ts
+++ implementations/data-structures/linked-lists/dll/barrett/index.ts
@@ -19,4 +19,18 @@ export default class DoublyLinkedList<T> {
         this.tail = newNode;
         this.length = 1;
     }
+
+    push(value: T): DoublyLinkedList<T> {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail!.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
