1c0436079a8103508377e0db1dadb7f7816a6961
stormymayday <stormymayday7@gmail.com>
2025-04-12 15:18:38 -0700

    barrett dll shift

--- implementations/data-structures/linked-lists/dll/barrett/index.ts
+++ implementations/data-structures/linked-lists/dll/barrett/index.ts
@@ -64,4 +64,21 @@ export default class DoublyLinkedList<T> {
         this.length++;
         return this;
     }
+
+    shift(): Node<T> | undefined {
+        if (this.length === 0) {
+            return undefined;
+        }
+        const temp = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head!.next;
+            this.head!.prev = null;
+            temp!.next = null;
+        }
+        this.length--;
+        return temp as Node<T>;
+    }
 }
