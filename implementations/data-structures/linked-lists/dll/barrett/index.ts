f5c88108cc67075d3d613ca04f76a892c2d11935
stormymayday <stormymayday7@gmail.com>
2025-04-12 17:18:22 -0700

    barrett dll get

--- implementations/data-structures/linked-lists/dll/barrett/index.ts
+++ implementations/data-structures/linked-lists/dll/barrett/index.ts
@@ -81,4 +81,22 @@ export default class DoublyLinkedList<T> {
         this.length--;
         return temp as Node<T>;
     }
+
+    get(index: number): Node<T> | undefined {
+        if (index < 0 || index >= this.length) {
+            return undefined;
+        }
+        let temp = this.head;
+        if (index < this.length / 2) {
+            for (let i = 0; i < index; i++) {
+                temp = temp!.next;
+            }
+        } else {
+            temp = this.tail;
+            for (let i = this.length - 1; i > index; i--) {
+                temp = temp!.prev;
+            }
+        }
+        return temp as Node<T>;
+    }
 }
