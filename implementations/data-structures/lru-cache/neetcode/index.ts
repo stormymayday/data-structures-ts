a9555113a78ac9f8849dcec014af8f0fd75a806e
stormymayday <stormymayday7@gmail.com>
2025-04-21 09:47:00 -0700

    neetcode lru constructor

--- implementations/data-structures/lru-cache/neetcode/index.ts
+++ implementations/data-structures/lru-cache/neetcode/index.ts
@@ -10,3 +10,23 @@ class Node<K, V> {
         this.prev = null;
     }
 }
+
+export default class LRUCache<K, V> {
+    capacity: number;
+    cache: Map<K, Node<K, V>>; // map key to node
+
+    // Dummy Nodes:
+    left: Node<K, V>; // LRU
+    right: Node<K, V>; // most recent
+
+    constructor(capacity: number) {
+        this.capacity = capacity;
+        this.cache = new Map();
+
+        this.left = new Node<K, V>(0 as K, 0 as V);
+        this.right = new Node<K, V>(0 as K, 0 as V);
+
+        this.left.next = this.right;
+        this.right.prev = this.left;
+    }
+}
